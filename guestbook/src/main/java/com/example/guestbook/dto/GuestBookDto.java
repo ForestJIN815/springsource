54f9def6d38b69904aedc0c7b124d9882b2491c8
진효찬 <attattahugen@gmail.com>
2024-11-15 18:16:27 +0900

    guestbook 완성, board 시작

---
+++ guestbook/src/main/java/com/example/guestbook/dto/GuestBookDto.java
@@ -0,0 +1,35 @@
+package com.example.guestbook.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+@Builder
+@Getter
+public class GuestBookDto {
+    private Long gno;
+
+    @NotBlank(message = "title 은 필수 입력 요소입니다.")
+    private String title;
+
+    @NotEmpty(message = "content 는 필수 입력 요소입니다.")
+    private String content;
+
+    @NotBlank(message = "writer 는 필수 입력 요소입니다.")
+    private String writer;
+
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+}
